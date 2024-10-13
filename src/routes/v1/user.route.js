be4f50fe1d041fcf711ad3ba9fe72ff05eaad495
cong.nguyenthanh <kong1032002@gmail.com>
2024-10-13 22:15:44 +0700

    remove auth

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -6,12 +6,10 @@ const userController = require('../../controllers/user.controller');
 
 const router = express.Router();
 
-router.route('/').post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser);
-
-// .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
-// .get('getUsers', validate(userValidation.getUsers), userController.getUsers);
-
-router.get('/', [auth('getUsers'), validate(userValidation.getUsers)], userController.getUsers);
+router
+  .route('/')
+  .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
+  .get([validate(userValidation.getUsers)], userController.getUsers);
 
 router
   .route('/:userId')
